1ee59f10fdb26d8b923ee95940dead7e9f19b5ea
danielbirhanu <danielbir49@gmail.com>
2025-03-14 08:28:20 +0300

    Create a message handler function

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,9 @@ chatForm.addEventListener("submit", (e) => {
 
   const msg = e.target.elements.msg.value;
   socket.emit("chatMessage", msg);
+
+  e.target.elements.msg.value = ''
+  e.target.elements.msg.focus()
 });
 
 function outPutMessage(message) {
